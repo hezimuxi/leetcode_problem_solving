5f4e19b19b337aa6666b6ecef32c80e0b2a5f5f0
Zining Qin <qinzining0906@foxmail.com>
2026-03-17 19:15:03 +0800

    Create 496.下一个更大元素-i.py

---
+++ 496.下一个更大元素-i.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=496 lang=python3
+#
+# [496] 下一个更大元素 I
+#
+
+# @lc code=start
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+       #爷就不用栈。你能把我怎么样
+        num=[]
+        for item in nums1:
+            i=0
+            while (nums2[i]!=item and i<len(nums2)):
+               i+=1
+             # 从 i+1 开始找下一个更大的
+            found = -1
+            for j in range(i + 1, len(nums2)):
+                if nums2[j] > item:
+                    found = nums2[j]
+                    break   
+            num.append(found)
+        return num
+
+            
+        
+# @lc code=end
+

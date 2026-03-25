19144a9fa5495dccf0eb8aa5da6c9839a9a593e0
Zining Qin <qinzining0906@foxmail.com>
2026-03-25 21:50:52 +0800

    Create 162.寻找峰值.py

---
+++ 162.寻找峰值.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=162 lang=python3
+#
+# [162] 寻找峰值
+#
+
+# @lc code=start
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        left = 0 
+        right = len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] < nums[mid + 1]:
+                left = mid + 1
+            else:
+                right = mid
+        return left
+    
+        
+        
+# @lc code=end
+
